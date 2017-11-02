41558d4a19ca4a5ca10379ad9c2536b3fe43d9ad
nslobodian <nazar.slobodian@7ninjas.com>
2017-11-02 17:13:05 +0100

    Get data from server

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
+const https = require('https')
+const trelloApi = 'https://api.trello.com/1/boards/N8KJDAYo'
+const key = 'your_key'
+const token = 'your_token'
+
 console.log('\n *START* \n')
 
 const argvs = process.argv
 let myYnitial = ''
 let jsonFile = ''
+let cards
+let members
 
-if (argvs.length > 3) {
+if (argvs.length > 2) {
   myYnitial = argvs[2]
-  jsonFile = argvs[3]
 } else {
-  throw new Error('No enough arguments. Need -myYnitial- -jsonFile-')
+  throw new Error('No enough arguments. Need -myYnitial-')
 }
 
-const obj = require(jsonFile)
-const cards = obj.cards
-const members = obj.members
+function parseCards (cards, members) {
+  console.log('\n *Start parsing cards* \n')
+  const myId = members.find(({ initials }) => initials === myYnitial).id
 
-const myId = members.find(({ initials }) => initials === myYnitial).id
+  const names = cards
+    .filter(({ dateLastActivity }) => new Date(dateLastActivity).toDateString() === new Date().toDateString())
+    .filter(({ idMembers }) => idMembers.length > 0 && idMembers.includes(myId))
+    .map(({ name, idShort }) => `[${idShort}]: ${name}`)
 
-const names = cards
-  .filter(({ dateLastActivity }) => new Date(dateLastActivity).toDateString() === new Date().toDateString())
-  .filter(({ idMembers }) => idMembers.length > 0 && idMembers.includes(myId))
-  .map(({ name, idShort }) => `[${idShort}]: ${name}`)
+  console.log(names.join(',\n'))
+  console.log('\n *EXIT* \n')
+}
 
-console.log(names.join(',\n'))
-console.log('\n *EXIT* \n')
+function getTrelloData(url) {
+  return new Promise((resolve, reject) => {
+    https.get(`${trelloApi}/${url}&key=${key}&token=${token}`, (res) => {
+      let rawData = ''
+      res.on('data', (chunk) => { rawData += chunk })
+      res.on('end', () => {
+        try {
+          console.log('\n *GET members* \n')
+
+          resolve(JSON.parse(rawData))
+        } catch (e) {
+          reject(e.message)
+        }
+      })
+    }).on('error', (e) => {
+      reject(e)
+    })
+  })
+}
+
+getTrelloData('cards?fields=dateLastActivity,idMembers,name,idShort').then(cards => {
+  console.log('\n *GET cards* \n')
+
+  getTrelloData('members?fields=initials,id').then(members => {
+    console.log('\n *GET members* \n')
+
+    parseCards(cards, members)
+  })
+})
